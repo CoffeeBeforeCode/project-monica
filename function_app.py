0cb310fc020723165d044f719478a0687d26b3ce
Phillip <dev@phillipcollins.com>
2026-03-04 14:53:51 +0000

    Remove unused blueprint registration

--- function_app.py
+++ function_app.py
@@ -3,13 +3,11 @@ import logging
 
 from task_chain import bp as bp_task_chain
 from webhook_renewal import bp as bp_renewal
-from task_creator import bp_creator
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 app.register_blueprint(bp_task_chain)
 app.register_blueprint(bp_renewal)
-app.register_blueprint(bp_creator)
 
 
 @app.route(route="ping", methods=["GET"])
